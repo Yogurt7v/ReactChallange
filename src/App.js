cf3d5343b3e9bcfa3d862b6deae34b0cbe4214ed
Yogurt7v <egorov-2k@yandex.ru>
2025-05-26 21:02:24 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-function List() {
-  const friends = ['Ben', 'Lynn', 'Alex'];
-
+function Badge({ name, handle, img }) {
   return (
-    <ul>
-      {friends.map((item, index) => (
-        <li key={index}>{item}</li>
-      ))}
-    </ul>
+    <div className="badge">
+      <img alt={name} src={img} />
+      <div>
+        <h4>{name}</h4>
+        <p>@{handle}</p>
+      </div>
+    </div>
   );
 }
 
 export default function App() {
-  return <List />;
+  return (
+    <Badge
+      name="Tyler McGinnis"
+      handle="tylermcginnis"
+      img="https://avatars0.githubusercontent.com/u/2933430"
+    />
+  );
 }
