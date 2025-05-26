e696c0524035e22fed573231f5d0b775eeab98fb
Yogurt7v <egorov-2k@yandex.ru>
2025-05-26 21:07:32 +0300

    object props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-function Badge({ name, handle, img }) {
+const USER_DATA = {
+  name: 'Ben Adam',
+  img: 'https://avatars.githubusercontent.com/u/6645985',
+  handle: 'benadam11',
+};
+
+function Badge({ user, style }) {
   return (
-    <div className="badge">
-      <img alt={name} src={img} />
+    <div style={style}>
+      <img alt={user.name} src={user.img} />
       <div>
-        <h4>{name}</h4>
-        <p>@{handle}</p>
+        <h4>{user.name}</h4>
+        <p>@{user.handle}</p>
       </div>
     </div>
   );
@@ -13,9 +19,20 @@ function Badge({ name, handle, img }) {
 export default function App() {
   return (
     <Badge
-      name="Tyler McGinnis"
-      handle="tylermcginnis"
-      img="https://avatars0.githubusercontent.com/u/2933430"
+      user={USER_DATA}
+      style={{
+        width: 300,
+        margin: '0 auto',
+        border: '1px solid var(--beige-10)',
+        borderRadius: 8,
+        backgroundColor: 'var(--charcoal)',
+        padding: 24,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: '16px',
+        textAlign: 'center',
+      }}
     />
   );
 }
