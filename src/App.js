9722cb787966ee8e946590ec361b0bd0106523eb
Yogurt7v <egorov-2k@yandex.ru>
2025-05-26 21:28:46 +0300

    e.target.value

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
-function Badge({ name, img, handle, children }) {
-  return (
-    <div className="badge">
-      <img alt={`Avatar for ${name}`} src={img} />
-      <header>
-        <h4>{name}</h4>
-        <h6>@{handle}</h6>
-      </header>
-      {children}
-    </div>
-  );
-}
-
 export default function App() {
-  const handleAddFriend = () => alert('Added');
+  function handleChange(e) {
+    if (e.target.value.length > 10) {
+      alert('Character limit exceeded');
+    }
+  }
 
   return (
-    <Badge
-      name="Alex Brown"
-      handle="alexbrown40"
-      img="https://avatars.githubusercontent.com/u/67877015"
-    >
-      <p>Alex enjoys running, surfing, and binge watching the Twilight series.</p>
-      <button onClick={handleAddFriend}>Add Friend</button>
-    </Badge>
+    <section>
+      <h1>Character Limit</h1>
+      <input placeholder="Enter some text" onChange={(e) => handleChange(e)} />
+    </section>
   );
 }
