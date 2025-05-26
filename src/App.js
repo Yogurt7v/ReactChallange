40721c221ea20fbc1adea55279e3b868df996ad2
Yogurt7v <egorov-2k@yandex.ru>
2025-05-26 21:22:07 +0300

    children props

--- src/App.js
+++ src/App.js
@@ -1,40 +1,27 @@
-function Avatar({ src, name }) {
-  return <img src={src} alt={name} />;
-}
-
-function Name({ name }) {
-  return <h4>{name}</h4>;
-}
-
-function Handle({ handle }) {
-  return <p>@{handle}</p>;
-}
-
-function Badge({ user, style }) {
+function Badge({ name, img, handle, children }) {
   return (
-    <div style={style}>
-      <Avatar src={user.img} name={user.name} />
-      <div>
-        <Name name={user.name} />
-        <Handle handle={user.handle} />
-        <button onClick={() => {}}>Add Friend</button>
-      </div>
+    <div className="badge">
+      <img alt={`Avatar for ${name}`} src={img} />
+      <header>
+        <h4>{name}</h4>
+        <h6>@{handle}</h6>
+      </header>
+      {children}
     </div>
   );
 }
 
 export default function App() {
+  const handleAddFriend = () => alert('Added');
+
   return (
     <Badge
-      user={{
-        name: 'Lynn Fisher',
-        img: 'https://avatars.githubusercontent.com/u/871315',
-        handle: 'lynnandtonic',
-      }}
-      style={{
-        width: 300,
-        margin: '0 auto',
-      }}
-    />
+      name="Alex Brown"
+      handle="alexbrown40"
+      img="https://avatars.githubusercontent.com/u/67877015"
+    >
+      <p>Alex enjoys running, surfing, and binge watching the Twilight series.</p>
+      <button onClick={handleAddFriend}>Add Friend</button>
+    </Badge>
   );
 }
