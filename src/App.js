d46c3d7243612cbd70004f144f55b4fbb082bf26
Yogurt7v <egorov-2k@yandex.ru>
2025-05-27 21:04:28 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,130 +1,53 @@
 import * as React from 'react';
 
-export function MultiStepForm() {
-  const [initialFormData, setInitialFormData] = React.useState({
-    name: '',
-    email: '',
-    address: '',
-    city: '',
-    zipcode: '',
-  });
-  const [currentStep, setCurrentStep] = React.useState(1);
+const items = [
+  'Apple',
+  'Banana',
+  'Cherry',
+  'Date',
+  'Fig',
+  'Grape',
+  'Honeydew',
+  'Lemon',
+  'Mango',
+  'Nectarine',
+  'Orange',
+  'Papaya',
+  'Raspberry',
+  'Strawberry',
+  'Watermelon',
+];
 
-  const handleChange = (e) => {
-    const { id, value } = e.target;
-    setInitialFormData({ ...initialFormData, [id]: value }); // тупанул здесь
-  };
+export function SearchFilter() {
+  const [searchTerm, setSearchTerm] = React.useState('');
+  const [filteredItems, setFilteredItems] = React.useState(items);
 
-  const handleNextStep = () => {
-    if (currentStep === 1) {
-      setCurrentStep(2);
-    }
-  };
-
-  const handlePrevStep = () => {
-    if (currentStep === 2) {
-      setCurrentStep(1);
-    }
-  };
-
-  const handleSubmit = () => {
-    alert('Thank you for your submission');
-    console.log(initialFormData);
-  };
-
-  if (currentStep === 1) {
-    return (
-      <form onSubmit={handleSubmit}>
-        <h2>Personal Information</h2>
-        <div>
-          <label>Step {currentStep} of 3</label>
-          <progress value={currentStep} max={3} />
-        </div>
-        <div>
-          <label htmlFor="name">Name</label>
-          <input
-            required
-            name="name"
-            id="name"
-            placeholder="Enter your name"
-            value={initialFormData.name}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input
-            required
-            name="email"
-            id="email"
-            type="email"
-            placeholder="Enter your email"
-            value={initialFormData.email}
-            onChange={handleChange}
-          />
-        </div>
-
-        <button type="button" className="secondary" onClick={handleNextStep}>
-          Next
-        </button>
-      </form>
+  React.useEffect(() => {
+    const result = items.filter((item) =>
+      item.toLowerCase().includes(searchTerm.toLowerCase())
     );
-  } else if (currentStep === 2) {
-    return (
-      <form onSubmit={handleSubmit}>
-        <h2>Address</h2>
-        <div>
-          <label>Step {currentStep} of 3</label>
-          <progress value={currentStep} max={3} />
-        </div>
-        <div>
-          <label htmlFor="address">Address</label>
-          <input
-            required
-            name="address"
-            id="address"
-            type="address"
-            placeholder="What is your address?"
-            value={initialFormData.address}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="city">City</label>
-          <input
-            required
-            name="city"
-            id="city"
-            placeholder="What city do you live in?"
-            value={initialFormData.city}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="zipcode">Zipcode</label>
-          <input
-            required
-            name="zipcode"
-            id="zipcode"
-            type="number"
-            placeholder="What is your zipcode?"
-            value={initialFormData.zipcode}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <button className="secondary" type="button" onClick={handleSubmit}>
-            Next
-          </button>
-          <button type="button" className="link" onClick={handlePrevStep}>
-            Previous
-          </button>
-        </div>
-      </form>
-    );
-  }
+    setFilteredItems(result);
+  }, [searchTerm]);
+
+  return (
+    <div>
+      <h1>Search Filter</h1>
+      <input
+        type="text"
+        placeholder="Search params"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+
+      <ul>
+        {filteredItems.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default function App() {
-  return MultiStepForm();
+  return SearchFilter();
 }
