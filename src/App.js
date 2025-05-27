f4ae6c0552b391db3fa3566bad4873783e699825
Yogurt7v <egorov-2k@yandex.ru>
2025-05-27 21:42:21 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
 import * as React from 'react';
 
-export function Clock() {
-  const [time, setTime] = React.useState(null);
-  React.useEffect(() => {
-    setInterval(() => {
-      setTime(new Date());
-    }, 1000);
-  }, [time, setTime]);
+export default function CountryInfo() {
+  const [countryCode, setCountryCode] = React.useState('AU');
+  const [data, setData] = React.useState({
+    data: null,
+    isLoading: true,
+    error: null,
+  });
 
-  if (time === null) return null;
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setCountryCode(value);
+  };
+
+  React.useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(`https://restcountries.com/v3.1/alpha/${countryCode}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }).then((r) => r.json());
+
+      return setData({ ...data, data: res[0], isLoading: false });
+    }
+    fetchData();
+  }, [countryCode, data]);
 
   return (
     <section>
-      <h1>Current Time</h1>
-      <p>{time.toLocaleTimeString()}</p>
+      <header>
+        <h1>Country Info:</h1>
+
+        <label htmlFor="country">Select a country:</label>
+        <div>
+          <select id="country" value={countryCode} onChange={handleChange}>
+            <option value="AU">Australia</option>
+            <option value="CA">Canada</option>
+            <option value="CN">China</option>
+            <option value="FR">France</option>
+            <option value="DE">Germany</option>
+            <option value="IN">India</option>
+            <option value="JP">Japan</option>
+            <option value="MX">Mexico</option>
+            <option value="GB">United Kingdom</option>
+            <option value="US">United States of America</option>
+          </select>
+          {data.isLoading && <span>Loading...</span>}
+          {data.error && <span>{data.error.message}</span>}
+        </div>
+      </header>
+      <p>Capital: {data.data.capital}</p>
+      <p>Population : {data.data.population.toLocaleString('ru-Ru')}</p>
     </section>
   );
-}
-
-export default function App() {
-  return Clock();
 }
