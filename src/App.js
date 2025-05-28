2417ea317113fe43eebff929fe5100a7a4548b9d
Yogurt7v <egorov-2k@yandex.ru>
2025-05-28 20:27:56 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,61 +1,100 @@
 import * as React from 'react';
 
-export default function CountryInfo() {
-  const [countryCode, setCountryCode] = React.useState('AU');
-  const [data, setData] = React.useState({
-    data: null,
-    isLoading: true,
-    error: null,
-  });
+const fetchData = async ({ query = '', page = 0, tag = '' }) => {
+  return fetch(
+    `https://hn.algolia.com/api/v1/search?query=${query}&tags=${encodeURIComponent(
+      tag
+    )}&page=${page}`
+  )
+    .then((response) => response.json())
+    .then((json) => ({
+      results: json.hits || [],
+      pages: json.nbPages || 0,
+      resultsPerPage: json.hitsPerPage || 20,
+    }));
+};
 
-  const handleChange = (e) => {
-    const { id, value } = e.target;
-    setCountryCode(value);
-  };
+export function HackerNewsSearch() {
+  const [query, setQuery] = React.useState('');
+  const [results, setResults] = React.useState([]);
+  const [tag, setTag] = React.useState('story');
+  const [page, setPage] = React.useState(0);
+  const [resultsPerPage, setResultsPerPage] = React.useState(0);
+  const [totalPages, setTotalPages] = React.useState(50);
+  const [loading, setLoading] = React.useState(false);
 
   React.useEffect(() => {
-    async function fetchData() {
-      const res = await fetch(`https://restcountries.com/v3.1/alpha/${countryCode}`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }).then((r) => r.json());
+    const loadResults = async () => {
+      setLoading(true);
+      const { pages, results, resultsPerPage } = await fetchData({ query, page, tag });
+      setResults(results);
+      setResultsPerPage(resultsPerPage);
+      setTotalPages(pages);
+    };
+    loadResults();
+  }, [query, page, tag]);
 
-      return setData({ ...data, data: res[0], isLoading: false });
-    }
-    fetchData();
-  }, [countryCode, data]);
+  const handleSearch = (e) => {
+    setQuery(e.target.value);
+    setPage(0);
+  };
+
+  const handleTag = (e) => {
+    setTag(e.target.value);
+    setPage(0);
+  };
+
+  const handleNextPage = () => {
+    setPage(page + 1);
+  };
+
+  const handlePrevPage = () => {
+    setPage(page - 1);
+  };
 
   return (
-    <section>
-      <header>
-        <h1>Country Info:</h1>
-
-        <label htmlFor="country">Select a country:</label>
+    <main>
+      <h1>Hacker News Search</h1>
+      <form onSubmit={(e) => e.preventDefault()}>
         <div>
-          <select id="country" value={countryCode} onChange={handleChange}>
-            <option value="AU">Australia</option>
-            <option value="CA">Canada</option>
-            <option value="CN">China</option>
-            <option value="FR">France</option>
-            <option value="DE">Germany</option>
-            <option value="IN">India</option>
-            <option value="JP">Japan</option>
-            <option value="MX">Mexico</option>
-            <option value="GB">United Kingdom</option>
-            <option value="US">United States of America</option>
-          </select>
-          {data.isLoading && <span>Loading...</span>}
-          {data.error && <span>{data.error.message}</span>}
+          <label htmlFor="query">Search</label>
+          <input
+            type="text"
+            id="query"
+            name="query"
+            value={query}
+            onChange={handleSearch}
+            placeholder="Search Hacker News..."
+          />
         </div>
-      </header>
-      {data.data && (
-        <>
-          <p>Capital: {data.data.capital}</p>
-          <p>Population : {data.data.population.toLocaleString('ru-Ru')}</p>
-        </>
-      )}
-    </section>
+        <div>
+          <label htmlFor="tag">Tag</label>
+          <select id="tag" name="tag" onChange={handleTag} value={tag}>
+            <option value="story">Story</option>
+            <option value="ask_hn">Ask HN</option>
+            <option value="show_hn">Show HN</option>
+            <option value="poll">Poll</option>
+          </select>
+        </div>
+      </form>
+      <section>
+        {!loading && <div>Loading</div>}
+        {results && (
+          <ul>
+            {results?.map((item) => (
+              <li>
+                <a href={item.url} key={item.story_id}>
+                  {item.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        )}
+      </section>
+    </main>
   );
+}
+
+export default function App() {
+  return HackerNewsSearch();
 }
