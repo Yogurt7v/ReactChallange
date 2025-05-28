bfec633a66733e1bb951af4f81a8ec1cbdc3c0a7
Yogurt7v <egorov-2k@yandex.ru>
2025-05-28 21:15:03 +0300

    done. тупанул немного

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
 import * as React from 'react';
 
-function TextInput() {
-  const ref = React.useRef(null);
+export function VideoPlayer() {
+  const [isPlaying, setIsPlaying] = React.useState(false);
+  const videoRef = React.useRef(null);
 
   React.useEffect(() => {
-    ref.current.focus();
-  }, []);
+    if (isPlaying) {
+      videoRef.current.play();
+    } else {
+      videoRef.current.pause();
+    }
+  }, [isPlaying]);
+
+  const handleTogglePlay = () => {
+    setIsPlaying(!isPlaying);
+  };
 
   return (
-    <div>
-      <h1>Autofocus Input</h1>
-      <label htmlFor="focus">Email Address</label>
-      <input id="focus" type="email" placeholder="Enter your email" ref={ref} />
-    </div>
+    <section className="container">
+      <h1>Video Player</h1>
+      <article>
+        <video
+          poster="https://image.mux.com/TbVCJiOghmISJgg4AznPfFHYRfiVoek8OJHF56Y01oR4/thumbnail.webp"
+          ref={videoRef}
+        >
+          <source
+            src="https://stream.mux.com/TbVCJiOghmISJgg4AznPfFHYRfiVoek8OJHF56Y01oR4/high.mp4"
+            type="video/mp4"
+          />
+          Ваш браузер не поддерживает видео.
+        </video>
+        <div>
+          <button title={isPlaying ? 'Pause' : 'Play'} onClick={handleTogglePlay}>
+            {isPlaying ? '⏸' : '▶'}
+          </button>
+        </div>
+      </article>
+    </section>
   );
 }
 
 export default function App() {
-  return TextInput();
+  return VideoPlayer();
 }
