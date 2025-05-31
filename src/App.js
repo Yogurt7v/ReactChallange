61773e57ac38063fb465c63c59ee5f65f7d48190
Yogurt7v <egorov-2k@yandex.ru>
2025-05-31 12:14:38 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,47 +1,28 @@
 import * as React from 'react';
 
-export function ClickOutside() {
-  const [isOpen, setIsOpen] = React.useState(false);
-  const dialog = React.useRef(null);
+export default function ExpandingTextarea() {
+  const [text, setText] = React.useState('');
+  const textZone = React.useRef(null);
 
-  React.useEffect(() => {
-    const handlePointer = (e) => {
-      if (e.target !== dialog.current) {
-        handleCloseModal();
-      }
-    };
-    document.addEventListener('pointerdown', (e) => handlePointer(e));
-
-    return () => document.removeEventListener('pointerdown', handlePointer);
-  }, []);
-
-  const handleOpenModal = () => {
-    setIsOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsOpen(false);
+  const handleChange = (e) => {
+    setText(e.target.value);
+    const textarea = textZone.current;
+    textarea.style.height = 'auto';
+    textarea.style.height = textarea.scrollHeight + 'px';
   };
 
   return (
-    <>
-      <section>
-        <h1>Click Outside</h1>
-        <button className="link" onClick={handleOpenModal}>
-          Open Modal
-        </button>
-      </section>
-      {isOpen && (
-        <dialog ref={dialog}>
-          <button onClick={handleCloseModal}>X</button>
-          <h2>Modal</h2>
-          <p>Click outside the modal to close (or use the button) whatever you prefer.</p>
-        </dialog>
-      )}
-    </>
+    <section className="container">
+      <h1>Expanding Textarea</h1>
+      <label htmlFor="textarea">Enter or paste in some text</label>
+      <textarea
+        ref={textZone}
+        id="textarea"
+        placeholder="Enter some text"
+        value={text}
+        onChange={handleChange}
+        rows={1}
+      />
+    </section>
   );
-}
-
-export default function App() {
-  return ClickOutside();
 }
