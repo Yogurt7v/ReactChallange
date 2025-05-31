375bbc78cc3d9974f0e93595a3c553a5ecd316e1
Yogurt7v <egorov-2k@yandex.ru>
2025-05-31 13:54:56 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,56 +1,93 @@
 import * as React from 'react';
 
-const translations = {
-  en: {
-    hello: 'Hello!',
-    welcome: 'Welcome to our app!',
-  },
-  es: {
-    hello: '¡Hola!',
-    welcome: '¡Bienvenido a nuestra aplicación!',
-  },
-  fr: {
-    hello: 'Bonjour !',
-    welcome: 'Bienvenue dans notre application !',
-  },
-  de: {
-    hello: 'Hallo!',
-    welcome: 'Willkommen in unserer App!',
-  },
-};
-
-const languageContext = React.createContext({
-  language: 'en',
-  changeLanguage: () => {},
-  translation: (key) => key,
+const videoPlaybackContext = React.createContext({
+  playingVideoId: null,
+  setPlayingVideoId: () => {},
 });
 
-function LanguageProvider() {
-  const [language, setLanguage] = React.useState(languageContext.language);
-  const [text, setText] = React.useState(Object.values(translations)[0]);
+function VideoPlaybackProvider({ children }) {
+  const playingVideoId = null;
+  const setPlayingVideoId = () => {};
 
-  const changeLanguage = (e) => {
-    setLanguage(e.target.value);
-  };
+  return children;
+}
 
-  const translation = () => {
-    setText(translations[language]);
+function VideoItem({ videoId, title, poster, src }) {
+  const [videoIsActive, setVideoIsActive] = React.useState(false);
+  const video = React.useRef(null);
+
+  const handleTogglePlay = () => {
+    const player = video.current;
+
+    if (!videoIsActive) {
+      player.play();
+      setVideoIsActive(true);
+    }
+    if (videoIsActive) {
+      player.pause();
+      setVideoIsActive(false);
+    }
   };
 
   return (
-    <>
-      <select defaultValue={languageContext.language} onChange={(e) => changeLanguage(e)}>
-        {Object.keys(translations).map((item) => (
-          <option value={item}>{item}</option>
+    <li>
+      <h3>{title}</h3>
+      <article>
+        <video poster={poster} ref={video}>
+          <source src={src} type="video/mp4" />
+        </video>
+        <button title={videoIsActive ? 'Pause' : 'Play'} onClick={handleTogglePlay}>
+          {videoIsActive ? '⏸' : '▶'}
+        </button>
+      </article>
+    </li>
+  );
+}
+
+function NewsFeed() {
+  const videos = [
+    {
+      id: 1,
+      title: 'The React Way',
+      poster: 'https://react.gg/img/visualized-og2.jpg',
+      src: 'https://stream.mux.com/TbVCJiOghmISJgg4AznPfFHYRfiVoek8OJHF56Y01oR4/high.mp4',
+    },
+    {
+      id: 2,
+      title: 'The History of the Web',
+      poster: 'https://react.gg/img/visualized-og1.jpg',
+      src: 'https://stream.mux.com/EwJPlEBa0046jGSVdYOnRsX9WnqHjytgIBXwkOt7LvVg/high.mp4',
+    },
+    {
+      id: 3,
+      title: 'Rendering, Visualized',
+      poster: 'https://react.gg/img/visualized-og5.jpg',
+      src: 'https://stream.mux.com/VvQKMwPEOq5BUnc9eRN4sL5sUEZrHqWxNlCbpXSkE3I/high.mp4',
+    },
+  ];
+
+  return (
+    <div>
+      <h1>News Feed</h1>
+      <ul>
+        {videos.map((item) => (
+          <VideoItem
+            key={item.id}
+            videoId={item.id}
+            title={item.title}
+            poster={item.poster}
+            src={item.src}
+          />
         ))}
-      </select>
-      <div>{text.hello}</div>
-      <div>{text.welcome}</div>
-      <button onClick={translation}>Translate</button>
-    </>
+      </ul>
+    </div>
   );
 }
 
 export default function App() {
-  return LanguageProvider();
+  return (
+    <VideoPlaybackProvider>
+      <NewsFeed />
+    </VideoPlaybackProvider>
+  );
 }
