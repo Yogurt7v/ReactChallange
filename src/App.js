25f11dd5b09d36c06d6f2daf5b1201a393f41124
Yogurt7v <egorov-2k@yandex.ru>
2025-06-03 21:25:10 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,70 +1,123 @@
 import * as React from 'react';
 
-function reducer(tasks, action) {
+const products = [
+  { id: 1, name: 'Poké Ball', price: 10 },
+  { id: 2, name: 'Great Ball', price: 20 },
+  { id: 3, name: 'Ultra Ball', price: 30 },
+];
+
+function calculateTotal(cart) {
+  const parsedItems = Array.isArray(cart) ? cart : [];
+  const total = parsedItems.reduce(
+    (current, item) => current + item.price * item.count,
+    0
+  );
+  return total;
+}
+
+const initialState = [];
+
+function reducer(cart, action) {
   switch (action.type) {
-    case 'add':
-      return [...tasks, { id: tasks.length + 1, task: action.task }];
+    case 'add': {
+      const inCart = Boolean(cart.find((item) => item.id === action.id));
 
-    case 'update':
-      console.log('update', action.id);
-      console.log('update', tasks);
-      return tasks.map((item) =>
-        item.id === action.id ? { ...item, status: 'done' } : item
-      );
+      if (!inCart) {
+        const add = products.find((item) => item.id === action.id);
+        return [...cart, { ...add, count: 1 }];
+      } else {
+        return cart.map((item) =>
+          item.id === action.id ? { ...item, count: item.count++ } : item
+        );
+      }
+    }
 
-    case 'delete':
-      console.log('delete');
-      return tasks.filter((item) => item.id !== action.id);
-
+    case 'update': {
+      if (action.adjustment === 'decrement') {
+        return cart
+          .map((item) =>
+            item.id === action.id && item.count > 0
+              ? { ...item, count: item.count - 1 }
+              : item
+          )
+          .filter((item) => item.count > 0);
+      }
+      if (action.adjustment === 'increment') {
+        return cart.map((item) =>
+          item.id === action.id ? { ...item, count: item.count + 1 } : item
+        );
+      }
+      break;
+    }
     default:
-      return tasks;
+      return cart;
   }
 }
 
-export default function TaskManager() {
-  const [tasks, dispatch] = React.useReducer(reducer, [{ id: 1, task: 'kcbakdjasbd' }]);
+export function ShoppingCart() {
+  const [cart, dispatch] = React.useReducer(reducer, initialState);
 
-  const createTask = (title) => {
-    return title;
+  const handleAddToCart = (id) => {
+    dispatch({ type: 'add', id });
+  };
+  const handleUpdateQuantity = (id, adjustment) => {
+    dispatch({
+      type: 'update',
+      id,
+      adjustment,
+    });
   };
 
-  const handleUpdateTaskStatus = (id) => {
-    dispatch({ type: 'update', id });
-  };
-
-  const handleDeleteTask = (id) => {
-    dispatch({ type: 'delete', id });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const form = document.getElementById('taskForm');
-    const formData = new FormData(form);
-    dispatch({ type: 'add', task: createTask(formData.get('task')) });
-
-    e.target.reset();
-  };
+  React.useEffect(() => {
+    calculateTotal(cart);
+  }, [cart]);
 
   return (
-    <div>
-      <h1>Task Manager</h1>
-      <form onSubmit={handleSubmit} id="taskForm">
-        <input name="task" placeholder="Task title" />
-        <button className="primary" type="submit">
-          Add Task
-        </button>
-      </form>
-
-      <ol>
-        {tasks.map((item, index) => (
-          <>
-            <li key={item.index}>{item.task}</li>
-            <button onClick={() => handleUpdateTaskStatus(item.id)}>V</button>
-            <button onClick={() => handleDeleteTask(item.id)}>X</button>
-            {item.status === 'done' && <p>Done</p>}
-          </>
-        ))}
-      </ol>
-    </div>
+    <main>
+      <h1>Poké Mart</h1>
+      <section>
+        <div>
+          <ul className="products">
+            {products.map((item) => (
+              <li key={item.id}>
+                {item.name} - {item.price} $
+                <button className="primary" onClick={() => handleAddToCart(item.id)}>
+                  Add to Chart
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
+      <br />
+      <aside>
+        <div>
+          <h2>Shopping Chart</h2>
+          <ul>
+            {cart.map((item) => (
+              <li key={item.id}>
+                {item.name} - {item.count}
+                <div>
+                  <button onClick={() => handleUpdateQuantity(item.id, 'decrement')}>
+                    -
+                  </button>
+                  {item.quantity}
+                  <button onClick={() => handleUpdateQuantity(item.id, 'increment')}>
+                    +
+                  </button>
+                </div>
+              </li>
+            ))}
+            {!cart.length && <li>Cart is empty</li>}
+          </ul>
+        </div>
+      </aside>
+      <br />
+      <div>Total - {calculateTotal(cart)}$</div>
+    </main>
   );
+}
+
+export default function App() {
+  return ShoppingCart();
 }
