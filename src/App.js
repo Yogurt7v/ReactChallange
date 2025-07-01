2e27a05016406135ef4aa0db256c03ce158f44af
Yogurt7v <egorov-2k@yandex.ru>
2025-07-01 21:30:27 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,49 +1,52 @@
-import * as React from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 
-export function useFavicon(newLink) {
-  React.useEffect(() => {
-    const favicon = document.querySelector("link[rel='icon']");
-    if (favicon) {
-      favicon.href = newLink;
-    }
-  }, [newLink]);
+function oldSchoolCopy(text) {
+  const tempTextArea = document.createElement('textarea');
+  tempTextArea.value = text;
+  document.body.appendChild(tempTextArea);
+  tempTextArea.select();
+  document.execCommand('copy');
+  document.body.removeChild(tempTextArea);
 }
 
+export function useCopyToClipboard() {
+  const [text, setText] = useState();
+
+  const handleCopy = useCallback((val) => {
+    const asyncCopy = async () => {
+      try {
+        if (navigator?.clipboard?.writeText) {
+          await navigator.clipboard.writeText(val);
+          setText(val);
+        } else {
+          throw new Error('No Navigator');
+        }
+      } catch (e) {
+        oldSchoolCopy(val);
+        setText(val);
+      }
+    };
+    asyncCopy();
+  }, []);
+
+  return [text, handleCopy];
+}
+
+const randomHash = crypto.randomUUID();
+
 export default function App() {
-  const [favicon, setFavicon] = React.useState(
-    'https://ui.dev/favicon/favicon-32x32.png'
-  );
-
-  useFavicon(favicon);
-
+  const [copiedText, copyToClipboard] = useCopyToClipboard();
+  console.log('Скопированный текст', copiedText);
   return (
     <section>
-      <h1>useFavicon</h1>
-
-      <button
-        title="Set the favicon to Bytes' logo"
-        className="link"
-        onClick={() => setFavicon('https://bytes.dev/favicon/favicon-32x32.png')}
-      >
-        Bytes
-      </button>
-      <button
-        title="Set the favicon to React Newsletter's logo"
-        className="link"
-        onClick={() =>
-          setFavicon('https://reactnewsletter.com/favicon/favicon-32x32.png')
-        }
-      >
-        React Newsletter
-      </button>
-
-      <button
-        title="Set the favicon to uidotdev's logo"
-        className="link"
-        onClick={() => setFavicon('https://ui.dev/favicon/favicon-32x32.png')}
-      >
-        ui.dev
-      </button>
+      <h1>useCopyToClipboard</h1>
+      <article>
+        <label>Fake API Key</label>
+        <pre>
+          <code>{randomHash}</code>
+          <button ton className="link" onClick={() => copyToClipboard(randomHash)} />
+        </pre>
+      </article>
     </section>
   );
 }
